201f18ccca5ce9e6a1b3534bcfac29c2572838f2
Christopher Smith <QueueHammer>
2015-10-27 00:57:38 -0400

    Update to allow recording of tasks and esecution of them

--- mockingbird.js
+++ mockingbird.js
@@ -1,8 +1,13 @@
-/* global document, localStorage */
+/* global document, localStorage, setTimeout */
 (function () {
   var usTag = 'us_' + (Math.random() * 100000000).toFixed();
   
+  var callBack = null;
+  var task = [];
+  var tasks = [];
+  
   function findSelector(target) {
+    if(!callBack) return;
     var now = target;
 
     var path = [];
@@ -26,7 +31,35 @@
       now = now.parentElement;
     }
 
-    return path.join(' > ');
+    callBack(path.join(' > '));
+    callBack = null;
+  }
+  
+  function executeTask(task) {
+    var step = 0;
+    
+    function click(target) {
+      console.log('clicking: ', target);
+      target.dispatchEvent(new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+      }));
+    }
+    
+    function takeStep() {
+      var target = document.querySelector(task[step].el);
+      if(target) click(target);
+      
+      step = step + 1;
+      if(step != task.length) setTimeout(takeStep);
+    }
+    
+    takeStep();
+  }
+  
+  function waitForClick(cb) {
+    setTimeout(function () { callBack = cb; });
   }
   
   function save (tasks) {
@@ -38,7 +71,10 @@
   }
   
   function bind() {
+    tasks = load() || [];
     document.addEventListener("DOMContentLoaded", function() {
+      document.addEventListener('click', function (e) { findSelector(e.target); });
+      
       var container = document.createElement('div');
       container.innerHTML = '<div style="position:fixed; top:0; right:0; width:150px; height:200px; background: #fff; border: 1px solid #000;">';
       document.body.appendChild(container);
@@ -50,22 +86,38 @@
   }
   
   function showTasks(el) {
+    task = [];
     el.innerHTML = '';
     el.innerHTML = [
       '<div style="overflow: auto;">',
         '<input type="text" placeholder="task name" style="float: left; width: 100px">',
         '<span class="add" style="float: right;">+</span>',
+      '</div>',
+      '<div class="row" style="display: none">',       
       '</div>'
+      
     ].join('');
     
     el.querySelector('.add')
     .addEventListener('click', function () {
       showAddTasks(el);
     });
+    
+    var row = el.querySelector('.row');
+    tasks.forEach(function (task) {
+      var newRow = row.cloneNode(true);
+      newRow.style.display = 'block';
+      newRow.innerText = task.name;
+      
+      newRow.addEventListener('click', function () {
+        executeTask(task.task);
+      });
+      el.appendChild(newRow);
+    });
   }
   
   function showAddTasks(el, name) {
-    if(!name) name = 'new task';
+    if(!name) { name = 'new task'; }
     el.innerHTML = '';
     
     el.innerHTML = [
@@ -78,9 +130,9 @@
         '</span>',
       '</div>',
       '<div>',
-        '<span>Add: </span><span class="click">click</span>',
+        '<span class="click">+click</span>',
       '</div>',
-      '<div class="click-task" style="display: none;">',
+      '<div class="click-task" style="display: none; height: 16px; overflow: hidden;">',
         '<span class="task-description"></span>',
       '</div>'
     ].join('');
@@ -91,16 +143,28 @@
       showTasks(el);
     });
     
+    el.querySelector('.done')
+    .addEventListener('click', function () {
+      tasks.push({ name: name, task: task } );
+      save(tasks);
+      showTasks(el);
+    });
+    
     el.querySelector('.click')
     .addEventListener('click', function () {
-      var newTask = el.querySelector('.click-task')
-      .cloneNode(true);
       
-      newTask.style.display = 'block';
-      newTask.querySelector('.task-description')
-      .innerText = 'something...';
-      
-      el.appendChild(newTask);
+      waitForClick(function (path) {
+        var newTask = el.querySelector('.click-task')
+        .cloneNode(true);
+
+        newTask.style.display = 'block';
+        newTask.querySelector('.task-description')
+        .innerText = path;
+
+        el.appendChild(newTask);
+        
+        task.push({ task: 'click', el: path });
+      });
     });
   }
 
